0dab7a156b95553cfce024a8d2c5f8f4b59a6e34
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2026-03-01 08:00:37 +0100

    |Correct casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { AppRoutes } from './routes/AppRoutes'
-import { Layout } from './components/layout/Layout'
+import { Layout } from './components/Layout/Layout'
 import { Toaster } from './components/ui/sonner'
 
 function App() {
